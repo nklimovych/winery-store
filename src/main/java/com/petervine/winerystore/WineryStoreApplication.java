6fc989d8e815dc18532f011b6995a91ede48464b
Nazar Klimovych <rksvjdbx@gmail.com>
2024-02-08 20:36:35 +0200

    Initialization commit

---
+++ src/main/java/com/petervine/winerystore/WineryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.petervine.winerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineryStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineryStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petervine/winerystore/WineryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petervine.winerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineryStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
